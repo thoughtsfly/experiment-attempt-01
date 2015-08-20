177d72d0384bf98a5c696d135e5d227ae23fb1c7
Aparnesh gaurav <aparneshgaurav@gmail.com>
2015-08-20 21:43:25 +0530

    Updated with db connection working code
    Updated with db connection working code

--- com/thoughtsfly/testdb/JavaClientH2.java
+++ com/thoughtsfly/testdb/JavaClientH2.java
@@ -1,1 +1,27 @@
-import com.h2.library
+package com.thoughtsfly.testdb;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JavaClientH2 {
+ public static void main(String[] args) throws SQLException, ClassNotFoundException{
+	 System.out.println("hello");
+	  Class.forName("org.h2.Driver");
+      Connection conn = DriverManager.getConnection("jdbc:h2:~/learnH2", "tfc", "tfc");
+      // add application code here
+      System.out.println(conn);
+      Statement statement = conn.createStatement();
+      ResultSet resultSet = statement.executeQuery("select * from firsttable");
+      String str = null;
+      
+      while(resultSet.next()){
+    	   str = resultSet.getInt(1)+resultSet.getString(2);
+      }
+      System.out.println(str);
+      conn.close();
+  }
+ }
+
